35aec44c9de657431e313786bb1abae3a8dc529e
Bruno Oliveira Alves <202504058125@alunos.estacio.br>
2025-04-12 00:50:23 +0000

    Criei o desafio supertrunfo novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,25 @@
 //Teste larissa
 
 int main() {
+
+    int populacao, pontos_turisticos = 25;
+    float area_km2, pib = 1500.00;
+    char estado;
+    char cidade, codigo[100] = "Sao Paulo";
+    
+
+    printf("Vamos começar o cadastro das cartas.\n");
+    printf("Nome da Cidade, Estado e o Numero de Pontos Turisticos: ");
+        scanf("%s, %c, %d", &cidade, &estado, &pontos_turisticos);
+    
+    printf("Agora digite a população total, Area total e o PIB");
+        scanf("%d, %f, %f", &populacao, &area_km2, &pib);
+
+    printf("Agora por último, digite o código da carta: ");
+        scanf("%s", &codigo);
+
+}
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -17,6 +36,3 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
-    return 0;
-}
